1a4e71d4deb9398080d196f92db2e3eba26f4c82
Richard T <77714442+CoffeeAddict93@users.noreply.github.com>
2021-11-06 11:33:26 -0500

    Update run_clm.py

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -46,16 +46,16 @@ from transformers import (
 )
 from transformers.testing_utils import CaptureLogger
 #from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version
-from transformers.utils.versions import require_version
+#from transformers.utils import check_min_version
+#from transformers.utils.versions import require_version
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.13.0.dev0")
+#check_min_version("4.13.0.dev0")
 
-require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/language-modeling/requirements.txt")
+#require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/language-modeling/requirements.txt")
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_CAUSAL_LM_MAPPING.keys())
